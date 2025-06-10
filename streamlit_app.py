8b8d2ffe3250c3c347b629349f11c5881608e5c3
tyevsb <104210991+tyevsb@users.noreply.github.com>
2025-06-10 11:55:57 +0000

    groq - key

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ st.write(
 # Ask user for their OpenAI API key via `st.text_input`.
 # Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
 # via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
-groq_api_key = st.text_input("OpenAI API Key", type="password")
+groq_api_key = st.secrets("GROQ_API_KEY")
 if not groq_api_key:
     st.info("Please add your OpenAI API key to continue.", icon="🗝️")
 else:
